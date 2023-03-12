5f4c9c11e07f86c63a131705c6d73a2ad96b5efd
Stephan Uhle <stephanu@gmx.net>
2023-03-12 23:58:51 +0100

    Made initial code testable.

--- PyMyGekko/PyMyGekko.py
+++ PyMyGekko/PyMyGekko.py
@@ -1,9 +1,18 @@
 import aiohttp
+from yarl import URL
 
 
 class PyMyGekko:
-    def __init__(self, username: str, apiKey: str, gekkoId: str) -> None:
-        self._url = "https://live.my-gekko.com/api/v1/var"
+    def __init__(
+        self,
+        username: str,
+        apiKey: str,
+        gekkoId: str,
+        scheme: str = "https",
+        host: str = "live.my-gekko.com",
+        port: int = None,
+    ) -> None:
+        self._url = URL.build(scheme=scheme, host=host, port=port)
         self._authentication_params = {
             "username": username,
             "key": apiKey,
@@ -13,16 +22,15 @@ class PyMyGekko:
     async def try_connect(self) -> int:
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                self._url, params=self._authentication_params
+                self._url.with_path("/api/v1/var"), params=self._authentication_params
             ) as resp:
-                print(resp.status)
-                print(await resp.text())
                 return resp.status
 
     async def read_data(self) -> None:
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                self._url + "/status", params=self._authentication_params
+                self._url.with_path("/api/v1/var/status"),
+                params=self._authentication_params,
             ) as resp:
                 self._data = await resp.json(content_type="text/plain")
 

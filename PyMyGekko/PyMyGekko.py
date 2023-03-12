f43bce9a28bc2bda7702451ebeedfd7a0846a3fb
Stephan Uhle <stephanu@gmx.net>
2023-03-12 14:55:45 +0100

    Added simple test.

--- PyMyGekko/PyMyGekko.py
+++ PyMyGekko/PyMyGekko.py
@@ -3,19 +3,37 @@ import aiohttp
 
 class PyMyGekko:
     def __init__(self, username: str, apiKey: str, gekkoId: str) -> None:
-        self.url = "https://live.my-gekko.com/api/v1/var"
-        self.username = username
-        self.apiKey = apiKey
-        self.gekkoId = gekkoId
-
-    async def try_connect(self):
-        params = {
-            "username": self.username,
-            "key": self.apiKey,
-            "gekkoid": self.gekkoId,
+        self._url = "https://live.my-gekko.com/api/v1/var"
+        self._authentication_params = {
+            "username": username,
+            "key": apiKey,
+            "gekkoid": gekkoId,
         }
+
+    async def try_connect(self) -> int:
         async with aiohttp.ClientSession() as session:
-            async with session.get(self.url, params=params) as resp:
+            async with session.get(
+                self._url, params=self._authentication_params
+            ) as resp:
                 print(resp.status)
                 print(await resp.text())
                 return resp.status
+
+    async def read_data(self) -> None:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                self._url + "/status", params=self._authentication_params
+            ) as resp:
+                self._data = await resp.json(content_type="text/plain")
+
+    def get_globals_network(self):
+        if self._data == None:
+            return None
+
+        result = {}
+        if self._data["globals"] and self._data["globals"]["network"]:
+            network_data = self._data["globals"]["network"]
+            for key in network_data:
+                result[key] = network_data[key]["value"]
+
+        return result
--- tests/test_pymygekko.py
+++ tests/test_pymygekko.py
@@ -1,3 +1,5 @@
+import pytest
+from decouple import config
 from PyMyGekko.PyMyGekko import PyMyGekko
 
 
@@ -6,3 +8,21 @@ def test_init():
 
     assert api != None
 
+
+@pytest.mark.asyncio
+async def test_try_connect():
+    api = PyMyGekko(config("USERNAME"), config("APIKEY"), config("GEKKOID"))
+
+    response_status = await api.try_connect()
+    assert response_status == 200
+
+
+@pytest.mark.asyncio
+async def test_get_globals_network():
+    api = PyMyGekko(config("USERNAME"), config("APIKEY"), config("GEKKOID"))
+
+    await api.read_data()
+    globals_network = api.get_globals_network()
+
+    assert globals_network != None
+    print(globals_network)
